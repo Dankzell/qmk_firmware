30001d4e6e39b9a4abdfbcee2d80b90e580e76fb
gustav forsell <gustav.forsell@live.se>
2026-03-08 10:56:05 +0100

    update lupin

--- keyboards/lupin/keymaps/default/keymap.c
+++ keyboards/lupin/keymaps/default/keymap.c
@@ -1,7 +1,9 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
+#include <stdbool.h>
 
 enum sofle_layers {
     _QWERTY
@@ -28,21 +30,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_LGUI, KC_LALT, KC_LCTL , KC_ENT , KC_ENT,             KC_SPC,   KC_SPC, KC_RCTL, KC_RALT, KC_RGUI
   )};
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if (clockwise) {
-    tap_code(KC_UP);
-  } else {
-    tap_code(KC_DOWN);
-  }
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    return false;
 }
 
-bool dip_switch_updvoid encoder_update_user(uint8_t index, bool clockwise) {
-  if (clockwise) {
-    tap_code(KC_UP);
-  } else {
-    tap_code(KC_DOWN);
-  }
-}ate_user(uint8_t index, bool active) { 
+bool dip_switch_update_user(uint8_t index, bool active) {
   switch (index) {
     case 0:
       if(active) { tap_code(KC_MUTE); } else {  }
